517b94805c0d7780ebb994275a1a9b7a1adf07be
Peppe <patrik.hallqvist@student.nbi-handelsakademin.se>
2023-05-24 23:01:05 +0200

    oi

--- app.js
+++ app.js
@@ -14,22 +14,21 @@ app.get('/', (req, res) => {
 app.get('/api/data', (req, res) => {
   const dataFilePath = path.join(__dirname, 'data.json');
 
-  // Read the data from the JSON file
   fs.readFile(dataFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading data file:', err);
-      res.status(500).json({ error: 'Failed to fetch data' });
-      return;
-    }
+      if (err) {
+        console.error('Error reading data file:', err);
+        res.status(500).json({ error: 'Failed to fetch data' });
+        return;
+      }
 
-    try {
-      const tableData = JSON.parse(data);
-      res.json(tableData);
-    } catch (parseErr) {
-      console.error('Error parsing JSON:', parseErr);
-      res.status(500).json({ error: 'Failed to fetch data' });
-    }
-  });
+      try {
+        const tableData = JSON.parse(data);
+        res.json(tableData);
+      } catch (parseErr) {
+        console.error('Error parsing JSON:', parseErr);
+        res.status(500).json({ error: 'Failed to fetch data' });
+      }
+    });
 });
 
 app.listen(port, ()=> console.info(`Listening on port ${port}`));
