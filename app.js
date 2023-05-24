41f091afa71bb79b782387a80c000f209e663cd9
Peppe <patrik.hallqvist@student.nbi-handelsakademin.se>
2023-05-24 22:37:10 +0200

    theBoys

--- app.js
+++ app.js
@@ -1,68 +1,35 @@
 // import
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 8080;
 
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/img', express.static(__dirname + 'public/img'));
-
-let data = [];
-fs.readFile('data.json', (err, jsonData) => {
-  if (!err) {
-    data = JSON.parse(jsonData);
-  }
-});
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile('views/index.html', { root: __dirname });
 });
 
-app.get('/data', (req, res) => {
-  res.json(data);
-});
+app.get('/api/data', (req, res) => {
+  const dataFilePath = path.join(__dirname, 'data.json');
 
-app.post('/submit', (req, res) => {
-  const score = req.body.score;
-  const name = req.body.name;
-  const lastname = req.body.lastname;
-
-  data.push({ score, name, lastname });
-
-  fs.writeFile('data.json', JSON.stringify(data), (err) => {
+  // Read the data from the JSON file
+  fs.readFile(dataFilePath, 'utf8', (err, data) => {
     if (err) {
-      console.error('Error writing to data file:', err);
+      console.error('Error reading data file:', err);
+      res.status(500).json({ error: 'Failed to fetch data' });
+      return;
+    }
+
+    try {
+      const tableData = JSON.parse(data);
+      res.json(tableData);
+    } catch (parseErr) {
+      console.error('Error parsing JSON:', parseErr);
+      res.status(500).json({ error: 'Failed to fetch data' });
     }
   });
-
-  res.redirect('/');
 });
-/*
-ejs stuff
-
-app.get('/views', (req, res)=> {
-    res.sendFile(path.join(__dirnamem, 'index.html'));
-});
-
-app.get('/views', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'about.html'));
-});
-
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-app.get('', (req, res)=> {
-    res.render('index', { text: 'This is EJS'})
-})
-
-app.get('/about', (req, res)=> {
-    res.render('about', { text: 'This page were made by a Cookie Monster', })
-})
-
-*/
 
 app.listen(port, ()=> console.info(`Listening on port ${port}`));
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,123 @@
+//SCORESNAMNDATA
+window.addEventListener('DOMContentLoaded', () => {
+  const tableBody = document.querySelector('#tableBody');
+  const addButton = document.querySelector('#addButton');
+  const fetchButton = document.querySelector('#fetchButton');
+  fetchButton.addEventListener('click', fetchData);
+
+  addButton.addEventListener('click', addRow);
+
+function fetchData() {
+  fetch('/api/data')
+    .then(response => response.json())
+    .then(data => {
+      renderRows(data);
+    })
+    .catch(err => {
+      console.error('Error fetching data:', err);
+    });
+}
+
+function renderRows(data) {
+    tableBody.innerHTML = ''; // Clear
+
+    data.forEach(row => {
+      const tableRow = createRow(row.score, row.name, row.lastName);
+      tableBody.appendChild(tableRow);
+    });
+}
+
+function createRow(score, name, lastName) {
+  const tableRow = document.createElement('tr');
+  const scoreCell = createCell(score);
+  const nameCell = createCell(name);
+  const lastNameCell = createCell(lastName);
+
+  tableRow.appendChild(scoreCell);
+  tableRow.appendChild(nameCell);
+  tableRow.appendChild(lastNameCell);
+
+  return tableRow;
+}
+
+function createCell(text) {
+  const cell = document.createElement('td');
+  cell.textContent = text;
+  return cell;
+}
+
+function addRow() {
+  const inputScore = document.querySelector('#inputScore');
+  const inputName = document.querySelector('#inputName');
+  const inputLastName = document.querySelector('#inputLastName');
+
+  const score = inputScore.value;
+  const name = inputName.value;
+  const lastName = inputLastName.value;
+
+  if (score && name && lastName) {
+    const tableRow = createRow(score, name, lastName);
+    tableBody.appendChild(tableRow);
+
+    inputScore.value = '';
+    inputName.value = '';
+    inputLastName.value = '';// Cleear
+  }
+}
+});
+
+
+/* 
+WTF FAILURE CODE, DATA FAILURE
+
+lass Xcore {
+  constructor(score, name, lastName) {
+      this.score = score;
+      this.name = name;
+      this.lastName = lastName;
+  }
+  urScoreName() {
+      return `${this.score} ${this.name} ${this.lastName}`;
+  }
+}
+//-----------------------------------------
+
+let data = [];
+
+document.getElementById('submit').addEventListener('click', () => {
+    const score = parseInt(document.getElementById('scoreInput').value);
+    const name = document.getElementById('nameInput').value;
+    const lastName = document.getElementById('lastNameInput').value;
+
+    data.push(new Xcore(score, name, lastName));
+
+    scoreToTable();
+});
+
+function scoreToTable() {
+  let scores = "<table><tr>";
+    for (let x in data[0]) {
+        scores += `<th>${x}</th>`;
+    }
+    data.forEach(data => {
+        scores += "<tr>";
+
+        for (let attr in data) {
+            scores += `<td>${data[attr]}</td>`
+        }
+        scores += "</tr>";
+    });
+    document.getElementById("tBody").innerHTML = scores + "</table>";
+}
+*/
+
+/*
+const score = document.getElementById('scoreInput');
+const name = document.getElementById('nameInput');
+const lastname = document.getElementById('lastNameInput');
+*/
+
+
 /*
 fetch("savedScores.json").then(function(response){
   return response.json();
@@ -13,18 +133,36 @@ fetch("savedScores.json").then(function(response){
             </tr>`;
   }
 })
-*/
-document.addEventListener('DOMContentLoaded', () => {
+
+/*document.addEventListener('DOMContentLoaded', () => {
   const submitButton = document.getElementById('submitButton');
 
 submitButton.addEventListener('click', () => {
-  const scoreInput = document.getElementById('scoreInput');
-  const nameInput = document.getElementById('nameInput');
-  const lastnameInput = document.getElementById('lastnameInput');
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const score = document.getElementById('scoreInput').value;
+  const name = document.getElementById('nameInput').value;
+  const lastname = document.getElementById('lastnameInput').value;
+  const tableBody = document.querySelector('tBody');
+  const row = document.createElement('tr');
 
   const score = scoreInput.value;
   const name = nameInput.value;
   const lastname = lastnameInput.value;
+  
+  row.innerHTML = `
+    <td>${score}</td>
+    <td>${name}</td>
+    <td>${lastname}</td>
+  `;
+
+  tableBody.appendChild(row);
+
+  document.getElementById('score').value = '';
+  document.getElementById('name').value = '';
+  document.getElementById('lastname').value = '';
+});
 
     fetch('/submit', {
       method: 'POST',
@@ -54,3 +192,6 @@ submitButton.addEventListener('click', () => {
         });
       });
 });
+
+WTF FAILURE CODE
+*/
