9e11d575e21bf9095600533f3b371e0bd412e90c
Peppe <patrik.hallqvist@student.nbi-handelsakademin.se>
2023-06-02 20:39:16 +0200

    erf

--- script.js
+++ script.js
@@ -91,46 +91,6 @@ function getDataTable() {
 }
 
 function newTable() {
-  const scoreInput = document.getElementById('inputScore');
-  const nameInput = document.getElementById('inputName');
-  const lastNameInput = document.getElementById('inputLastName');
-
-  const score = scoreInput.value;
-  const name = nameInput.value;
-  const lastName = lastNameInput.value;
-
-  if (score && name && lastName) {
-    const newData = {
-      score: score,
-      name: name,
-      lastName: lastName
-    };
-
-    fetch('/data.json')
-      .then(response => response.json())
-      .then(data => {
-        const updatedData = [...data, newData];
-        return fetch('/data.json', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(updatedData)
-        });
-      })
-      .then(response => response.json())
-      .then(result => {
-        console.log('Data submitted successfully:', result);
-        renderRows(result); // Render the rows after submitting the data
-      })
-      .catch(err => {
-        console.error('Error submitting data:', err);
-      });
-  }
-}
-
-/*
-function newTable() {
   const data = getDataTable();
 
   fetch('data.json', {
@@ -143,13 +103,11 @@ function newTable() {
     .then(response => response.json())
     .then(result => {
       console.log('Data submitted successfully:', result);
-      renderRows(data);
     })
     .catch(err => {
       console.error('Error submitting data:', err);
     });
 }
-*/
 });
 
 /*
